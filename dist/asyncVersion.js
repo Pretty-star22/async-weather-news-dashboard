a2d3d0a4a3b53c83b593763bcbf93cd29d7f037c
pretty peakanyo <peakanyopretty@gmail.com>
2025-09-09 23:53:27 +0200

    created the different scripts

--- dist/asyncVersion.js
+++ dist/asyncVersion.js
@@ -1,10 +1,7 @@
-
-
+"use strict";
 // async function fetchData(){
 //       const promise1 = fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=cfc868854fc1f8db2118de2e4e5dae99");
 //       const promise2 = fetch("fetch('https://dummyjson.com/posts/1")
-      
-
 //     try{
 //      const [response1, response2] = await Promise.all([promise1,promise2]);
 //      const weatherData= await response1.json();
@@ -12,8 +9,5 @@
 //    }catch(err){
 //     console.log(err)
 //    }
-       
-    
-   
 // }
 console.log("Pretty");
