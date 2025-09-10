131c05bc1e120659953b1a764994576828fa24ab
pretty peakanyo <peakanyopretty@gmail.com>
2025-09-10 16:21:15 +0200

    fetching weather data using callback version

--- dist/callbackVersion.js
+++ dist/callbackVersion.js
@@ -1,2 +1,45 @@
 "use strict";
-console.log("callback version");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const https_1 = __importDefault(require("https"));
+const server = http_1.default.createServer((req, res) => {
+    res.end('data fetching');
+});
+server.listen(3000, () => {
+    console.log('server running at http;//localhost:3000/');
+});
+console.log("Pretty");
+function callbackWeather(callback) {
+    let city = "johannesburg";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cfc868854fc1f8db2118de2e4e5dae99`;
+    https_1.default.get(apiUrl, (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+        response.on('end', () => {
+            if (response.statusCode === 200) {
+                try {
+                    const weatherData = JSON.parse(data);
+                    callback(null, weatherData);
+                    // console.log(weatherData);
+                }
+                catch (error) {
+                    callback(new Error('failed to parse json response'));
+                }
+            }
+            else {
+                callback(new Error(`HTTP error! status:${response.statusCode}`));
+            }
+        }).on('error', (error) => {
+            callback(error);
+        });
+    });
+}
+callbackWeather((err, data) => {
+    console.log(data);
+    console.log(err);
+});
--- src/callbackVersion.ts
+++ src/callbackVersion.ts
@@ -1,1 +1,49 @@
-console.log("callback version");
+import http from 'http';
+import https from 'https';
+
+const server = http.createServer((req,res) => {
+    res.end('data fetching')
+})
+server.listen(3000, () => {
+    console.log('server running at http;//localhost:3000/');
+})
+console.log("Pretty");
+
+function callbackWeather(callback:(error:Error | null, weatherData?:any) => void){
+    let city = "johannesburg";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cfc868854fc1f8db2118de2e4e5dae99`
+   
+    https.get(apiUrl, (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
+        })
+          response.on('end',() => {
+            if(response.statusCode === 200){
+                try {
+                    const weatherData = JSON.parse(data);
+                   callback(null, weatherData)
+                  
+                } catch (error) {
+                    callback(new Error('failed to parse json response'))
+                    
+                }
+            }else{
+                 callback(
+                   new Error(`HTTP error! status:${response.statusCode}`)
+                 );
+            }
+          }).on('error', (error) => {
+            callback(error)
+          })
+    })
+    
+}
+callbackWeather((err, data) => {
+    if(err){
+      console.log(err);
+    }else{
+    console.log(data);
+    }
+    
+})
