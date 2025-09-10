caa0cb87fe7c8fc96411d7943b02934c68ff8be4
pretty peakanyo <peakanyopretty@gmail.com>
2025-09-10 16:47:13 +0200

    fetched news data using callbacks

--- dist/callbackVersion.js
+++ dist/callbackVersion.js
@@ -11,10 +11,10 @@ const server = http_1.default.createServer((req, res) => {
 server.listen(3000, () => {
     console.log('server running at http;//localhost:3000/');
 });
-console.log("Pretty");
 function callbackWeather(callback) {
     let city = "johannesburg";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cfc868854fc1f8db2118de2e4e5dae99`;
+    let newsUrl = "https://dummyjson.com/posts/1";
     https_1.default.get(apiUrl, (response) => {
         let data = '';
         response.on('data', (chunk) => {
@@ -25,7 +25,6 @@ function callbackWeather(callback) {
                 try {
                     const weatherData = JSON.parse(data);
                     callback(null, weatherData);
-                    // console.log(weatherData);
                 }
                 catch (error) {
                     callback(new Error('failed to parse json response'));
@@ -39,7 +38,46 @@ function callbackWeather(callback) {
         });
     });
 }
-callbackWeather((err, data) => {
-    console.log(data);
-    console.log(err);
+function callbackNews(callback) {
+    let newsUrl = "https://dummyjson.com/posts/1";
+    https_1.default.get(newsUrl, (response) => {
+        let data = "";
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+        response
+            .on("end", () => {
+            if (response.statusCode === 200) {
+                try {
+                    const Data = JSON.parse(data);
+                    callback(null, Data);
+                }
+                catch (error) {
+                    callback(new Error("failed to parse json response"));
+                }
+            }
+            else {
+                callback(new Error(`HTTP error! status:${response.statusCode}`));
+            }
+        })
+            .on("error", (error) => {
+            callback(error);
+        });
+    });
+}
+callbackWeather((err, weatherData) => {
+    callbackNews((err, NewsData) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(NewsData);
+        }
+    });
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(weatherData);
+    }
 });
--- src/callbackVersion.ts
+++ src/callbackVersion.ts
@@ -7,12 +7,13 @@ const server = http.createServer((req,res) => {
 server.listen(3000, () => {
     console.log('server running at http;//localhost:3000/');
 })
-console.log("Pretty");
+
 
 function callbackWeather(callback:(error:Error | null, weatherData?:any) => void){
     let city = "johannesburg";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cfc868854fc1f8db2118de2e4e5dae99`
-   
+    let newsUrl = "https://dummyjson.com/posts/1";
+
     https.get(apiUrl, (response) => {
         let data = '';
         response.on('data', (chunk) => {
@@ -37,13 +38,52 @@ function callbackWeather(callback:(error:Error | null, weatherData?:any) => void
             callback(error)
           })
     })
-    
+
 }
-callbackWeather((err, data) => {
-    if(err){
-      console.log(err);
-    }else{
-    console.log(data);
-    }
-    
-})
+
+
+
+function callbackNews(callback: (error: Error | null, NewsData?: any) => void) {
+
+  let newsUrl = "https://dummyjson.com/posts/1";
+
+  https.get(newsUrl, (response) => {
+    let data = "";
+    response.on("data", (chunk) => {
+      data += chunk;
+    });
+    response
+      .on("end", () => {
+        if (response.statusCode === 200) {
+          try {
+            const Data = JSON.parse(data);
+            callback(null, Data);
+          } catch (error) {
+            callback(new Error("failed to parse json response"));
+          }
+        } else {
+          callback(new Error(`HTTP error! status:${response.statusCode}`));
+        }
+      })
+      .on("error", (error) => {
+        callback(error);
+      });
+  });
+}
+
+callbackWeather((err, weatherData) => {
+    callbackNews((err, NewsData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(NewsData);
+      }
+    });
+   if (err) {
+     console.log(err);
+   } else {
+     console.log(weatherData);
+   }
+});
+
+
